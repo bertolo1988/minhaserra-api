87b87e0bce4807cdb1060b80b6c3109ec326dc1e
Tiago Bértolo <tiagobertolo@gmail.com>
2024-02-09 01:05:22 +0000

    Add router prefix.

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,9 @@ import { HealthController } from './controllers/health';
 import { UsersController, UsersValidator } from './controllers/users';
 
 export function configureKoaRouter(): Router {
-  const router = new Router();
+  const router = new Router({
+    prefix: '/api',
+  });
   router.get('/health', HealthController.hello);
   router.post(
     '/users',
