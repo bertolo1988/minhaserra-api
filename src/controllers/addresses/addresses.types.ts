7d58dd811df60e59ed3a0e51ed29beccfd24e4af
Tiago Bértolo <tiagobertolo@gmail.com>
2024-03-13 00:35:35 +0000

    Add addresses create types.

---
+++ src/controllers/addresses/addresses.types.ts
@@ -0,0 +1,36 @@
+export type CreateAddressDto = {
+  label: string;
+  countryCode: string;
+  name: string;
+  line1: string;
+  line2?: string;
+  city: string;
+  region: string;
+  postalCode: string;
+  phoneNumber?: string;
+};
+
+export const CreateAddressDtoSchema = {
+  type: 'object',
+  properties: {
+    label: { type: 'string', nullable: false },
+    countryCode: { type: 'country-code', nullable: false },
+    name: { type: 'string', nullable: false },
+    line1: { type: 'string', nullable: false },
+    line2: { type: 'string', nullable: true },
+    city: { type: 'string', nullable: false },
+    region: { type: 'string', nullable: false },
+    postalCode: { type: 'string', nullable: false },
+    phoneNumber: { type: 'string', nullable: true },
+  },
+  required: [
+    'label',
+    'countryCode',
+    'name',
+    'line1',
+    'city',
+    'region',
+    'postalCode',
+  ],
+  additionalProperties: false,
+};
