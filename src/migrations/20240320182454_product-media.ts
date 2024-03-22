4e5e03578555782360cbde49b1bb0f8848e41cee
Tiago Bértolo <tiagobertolo@gmail.com>
2024-03-22 23:10:27 +0000

    Separate product images and video.

--- src/migrations/20240320182454_product-media.ts
+++ src/migrations/20240320182454_product-media.ts
@@ -1,22 +1,33 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw(`CREATE TYPE media_type AS ENUM ('image','video');`);
-  await knex.schema.createTable('products_media', function (table) {
+  await knex.schema.createTable('product_images', function (table) {
     table
       .uuid('id')
       .unique()
       .defaultTo(knex.raw('gen_random_uuid()'))
       .primary();
     table.uuid('product_id').notNullable();
-    table.specificType('type', 'media_type').notNullable();
     table.text('url').notNullable();
+    table.text('description').nullable();
+    table.foreign('product_id').references('products.id').onDelete('CASCADE');
+    table.timestamps(true, true);
+  });
+  await knex.schema.createTable('product_videos', function (table) {
+    table
+      .uuid('id')
+      .unique()
+      .defaultTo(knex.raw('gen_random_uuid()'))
+      .primary();
+    table.uuid('product_id').notNullable();
+    table.text('url').notNullable();
+    table.text('description').nullable();
     table.foreign('product_id').references('products.id').onDelete('CASCADE');
     table.timestamps(true, true);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('products_media');
-  await knex.raw(`DROP TYPE IF EXISTS media_type;`);
+  await knex.schema.dropTable('product_videos');
+  await knex.schema.dropTable('product_images');
 }
