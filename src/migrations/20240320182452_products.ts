982a88c4fb952ea12499095d8d9716b4fa651a76
Tiago Bértolo <tiagobertolo@gmail.com>
2024-03-28 23:44:33 +0000

    Add type currency.

--- src/migrations/20240320182452_products.ts
+++ src/migrations/20240320182452_products.ts
@@ -1,6 +1,9 @@
 import type { Knex } from 'knex';
 
+const CURRENCIES = ['USD', 'EUR', 'GBP', 'JPY', 'CNY', 'CAD', 'AUD'];
+
 export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`CREATE TYPE currency AS ENUM ('${CURRENCIES.join("','")}');`);
   await knex.schema.createTable('products', function (table) {
     table.uuid('id').defaultTo(knex.raw('gen_random_uuid()')).primary();
     table.uuid('user_id').notNullable();
@@ -12,6 +15,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string('region', 100).nullable();
     table.integer('avaliable_quantity').notNullable().defaultTo(0);
     table.bigInteger('price').notNullable();
+    table.specificType('currency', 'currency').notNullable().defaultTo('EUR');
     table.boolean('is_on_sale').notNullable().defaultTo(true);
     table.boolean('is_deleted').notNullable().defaultTo(false);
     table.boolean('is_approved').notNullable().defaultTo(false);
@@ -21,4 +25,5 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable('products');
+  await knex.raw(`DROP TYPE IF EXISTS currency;`);
 }
