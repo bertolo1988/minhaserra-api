c517b96d82890960f51d04762635d22fe7c37094
Tiago Bértolo <tiagobertolo@gmail.com>
2024-03-30 22:11:54 +0000

    Add product update by id test suite.

--- test/integration/all-integration-tests.spec.ts
+++ test/integration/all-integration-tests.spec.ts
@@ -11,18 +11,23 @@ describe('Integration tests', () => {
     (await TestServerSingleton.getInstance()).stop();
   });
 
-  require('./contact-verifications/verify-user-email.spec');
   require('./health/health.spec');
+
+  require('./contact-verifications/verify-user-email.spec');
+
   require('./users/create-user.spec');
   require('./users/login.spec');
-  require('./password-resets/create-password-reset.spec');
-  require('./password-resets/update-password-unauthenticated.spec');
   require('./users/get-user-by-id.spec');
   require('./users/update-user.spec');
+
+  require('./password-resets/create-password-reset.spec');
+  require('./password-resets/update-password-unauthenticated.spec');
+
   require('./addresses/create-address.spec');
   require('./addresses/get-address-by-id.spec');
   require('./addresses/get-addresses.spec');
   require('./addresses/delete-address.spec');
+
   require('./products/create-product-image.spec');
   require('./products/get-product-images.spec');
   require('./products/delete-product-image.spec');
@@ -30,4 +35,5 @@ describe('Integration tests', () => {
   require('./products/get-product-by-id.spec');
   require('./products/delete-product-by-id.spec');
   require('./products/get-products.spec');
+  require('./products/update-product-by-id.spec');
 });
