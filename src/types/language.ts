f2cf836def4cf3beb98b5214f082c0c5a1ea301a
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-06 00:18:07 +0100

    Add languages.

---
+++ src/types/language.ts
@@ -0,0 +1,13 @@
+// based on https://docs.aws.amazon.com/translate/latest/dg/what-is-languages.html
+export enum Language {
+  PORTUGUESE = 'pt-PT',
+  ENGLISH = 'en',
+  SPANISH = 'es',
+  FRENCH = 'fr',
+  GERMAN = 'de',
+  ITALIAN = 'it',
+  DUTCH = 'nl',
+  JAPANESE = 'ja',
+  CHINESE = 'zh',
+  SWEDISH = 'sv',
+}
