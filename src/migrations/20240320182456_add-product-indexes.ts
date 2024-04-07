be16f5dc4fb65d540defbb17d6ec56aa45647f44
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-07 22:40:42 +0100

    Add indexes on products.

---
+++ src/migrations/20240320182456_add-product-indexes.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(
+    `CREATE INDEX IF NOT EXISTS search_document_idx on products using gin(search_document);`,
+  );
+  await knex.raw(
+    `CREATE INDEX IF NOT EXISTS name_idx on products using btree(name);`,
+  );
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw('drop index if exists search_document_idx;');
+  await knex.raw(`CREATE INDEX name_idx on products using btree(name);`);
+}
