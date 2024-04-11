2779b810a95cd3c70396d8952eb8d8a1bf88f11d
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-12 00:38:02 +0100

    Add products index by price.

--- src/migrations/20240320182456_add-product-indexes.ts
+++ src/migrations/20240320182456_add-product-indexes.ts
@@ -7,9 +7,13 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(
     `CREATE INDEX IF NOT EXISTS name_idx on products using btree(name);`,
   );
+  await knex.raw(
+    `CREATE INDEX IF NOT EXISTS price_idx on products using btree(price);`,
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.raw('drop index if exists search_document_idx;');
   await knex.raw(`drop index if exists name_idx;`);
+  await knex.raw(`drop index if exists price_idx;`);
 }
