01e6be5f8dc02d4af758811a86754e2dbd1a36c6
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-14 00:38:43 +0100

    Add shopping cart migration.

---
+++ src/migrations/20240413232841_add-shopping-cart-items.ts
@@ -0,0 +1,19 @@
+import type { Knex } from 'knex';
+
+const CARTS_TABLE_NAME = 'shopping_cart_items';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(CARTS_TABLE_NAME, function (table) {
+    table.uuid('id').defaultTo(knex.raw('gen_random_uuid()')).primary();
+    table.uuid('user_id').notNullable();
+    table.uuid('product_id').notNullable();
+    table.integer('quantity').notNullable().defaultTo(1);
+    table.foreign('user_id').references('users.id').onDelete('CASCADE');
+    table.foreign('product_id').references('products.id').onDelete('CASCADE');
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable(CARTS_TABLE_NAME);
+}
