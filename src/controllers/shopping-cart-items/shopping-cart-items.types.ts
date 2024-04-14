272d23895ff98e0ee7387b5c39a76fa49a8502e1
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-14 10:51:16 +0100

    Add schema.

--- src/controllers/shopping-cart-items/shopping-cart-items.types.ts
+++ src/controllers/shopping-cart-items/shopping-cart-items.types.ts
@@ -1,3 +1,5 @@
+import CONSTANTS from '../../constants';
+
 export type ShoppingCartItemModel = {
   id: string;
   userId: string;
@@ -10,4 +12,23 @@ export type ShoppingCartItemModel = {
 export type CreateShoppingCartItemDto = {
   productId: string;
   quantity: number;
+};
+
+export const CreateShoppingCartItemDtoSchema = {
+  type: 'object',
+  properties: {
+    quantity: {
+      nullable: false,
+      type: 'number',
+      minimum: 1,
+      maximum: CONSTANTS.MAX_AVAILABLE_QUANTITY,
+    },
+    productId: {
+      nullable: false,
+      type: 'string',
+      format: 'uuid',
+    },
+  },
+  required: ['quantity', 'productId'],
+  additionalProperties: false,
 };
