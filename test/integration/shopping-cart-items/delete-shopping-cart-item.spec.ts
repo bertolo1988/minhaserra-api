91a59069ecbdeeea3373ac70c9391313350d8d9e
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-15 00:39:18 +0100

    Add 2 tests.

--- test/integration/shopping-cart-items/delete-shopping-cart-item.spec.ts
+++ test/integration/shopping-cart-items/delete-shopping-cart-item.spec.ts
@@ -5,6 +5,14 @@ describe('DELETE /api/shopping_cart_items/:id', () => {
     test('when id is not a valid uuid', async () => {});
   });
 
+  describe('should return 401', () => {
+    test('if authorization is not provided', async () => {});
+  });
+
+  describe('should return 403', () => {
+    test('if user is a seller, endpoint is only for buyers', async () => {});
+  });
+
   describe('should return 404', () => {
     test('when item is not found', async () => {});
   });
