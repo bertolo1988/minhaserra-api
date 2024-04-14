114c2dfd16b065637c4ebb02fd8b2a7906ea5c41
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-14 10:50:43 +0100

    Draft controller.

--- src/controllers/shopping-cart-items/shopping-cart-items.controller.ts
+++ src/controllers/shopping-cart-items/shopping-cart-items.controller.ts
@@ -2,7 +2,13 @@ import Koa from 'koa';
 
 export class ShoppingCartItemsController {
   static async createShoppingCartItem(ctx: Koa.Context, next: Koa.Next) {
-    // TODO: Implement thiss
-    await next();
+    const { productId, quantity } = ctx.request.body;
+
+    // decrease available quantity of product
+    // create shopping cart item
+    const shoppingCartItemId = 'shopping-cart-item-id';
+
+    ctx.status = 201;
+    ctx.body = { id: shoppingCartItemId };
   }
 }
