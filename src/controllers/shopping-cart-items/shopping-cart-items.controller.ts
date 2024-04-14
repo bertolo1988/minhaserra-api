1d80ed0281b6fe849c716168328d267feaffd407
Tiago Bértolo <tiagobertolo@gmail.com>
2024-04-15 00:38:31 +0100

    Write test cases.

--- src/controllers/shopping-cart-items/shopping-cart-items.controller.ts
+++ src/controllers/shopping-cart-items/shopping-cart-items.controller.ts
@@ -18,10 +18,10 @@ export class ShoppingCartItemsController {
 
     if (isDeleteSuccessfull(deleteResult)) {
       ctx.status = 200;
-      ctx.body = { message: 'Item deleted successfully' };
+      ctx.body = { message: 'Shopping cart item deleted successfully' };
     } else {
       ctx.status = 404;
-      ctx.body = { message: 'Item not found' };
+      ctx.body = { message: 'Shopping cart item not found' };
     }
   }
 
---
+++ test/integration/shopping-cart-items/delete-shopping-cart-item.spec.ts
@@ -0,0 +1,15 @@
+// TODO: implement these tests
+
+describe('DELETE /api/shopping_cart_items/:id', () => {
+  describe('should return 400', () => {
+    test('when id is not a valid uuid', async () => {});
+  });
+
+  describe('should return 404', () => {
+    test('when item is not found', async () => {});
+  });
+
+  describe('should return 200', () => {
+    test('when item is successfully removed', async () => {});
+  });
+});
